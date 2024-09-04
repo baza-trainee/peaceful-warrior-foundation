7915a9ab462ed38adca62c88a990ae83e2b52993
GannaKov <zlatta2000@gmail.com>
2024-09-04 20:06:38 +0200

    change home_>about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-
-import Directions from '@/sections/about/Directions/Directions';
-import Hero from '@/sections/about/Hero/Hero';
-import Mission from '@/sections/about/Mission/Mission';
-import SupportUs from '@/sections/about/SupportUs/SupportUs';
-import Team from '@/sections/about/Team/Team';
-
 export default function Home() {
   return (
     <>
       <h1>Home Page</h1>
-      <Hero />      {/* ? */}
-      <Mission />   {/* ? */}
-      <Directions/> {/*  Віка */}
-      <Team/>       {/*  Влад */}
-      <SupportUs/>  {/*  Ганна */}
     </>
   );
 }
