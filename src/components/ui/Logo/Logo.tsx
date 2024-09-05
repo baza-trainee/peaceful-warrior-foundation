f87dd2accf0e521d8407ae0508bebf0a2cc39d38
NataliaMahera <magera.nataliia@gmail.com>
2024-09-05 22:08:12 +0200

    add: logo

---
+++ src/components/ui/Logo/Logo.tsx
@@ -0,0 +1,19 @@
+
+import Link from 'next/link';
+import { ICONS } from '../../../constants/icons/icons';
+
+export interface LogoProps {
+  type: 'dark' | 'light';
+}
+
+export const Logo: React.FC<LogoProps> = ({type}) => {
+  const LogoComponent = type === 'dark' ? ICONS.LOGO_DARK : ICONS.LOGO_LIGHT
+
+  return (
+    <Link href='/' aria-label='logo'>
+     <LogoComponent
+        className="h-[74px] lg:h-[136px]" 
+      />
+    </Link>
+  );
+};
