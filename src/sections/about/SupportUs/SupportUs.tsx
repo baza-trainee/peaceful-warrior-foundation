2628a625527c15f0627653bc6e37b25568cfc48b
GannaKov <zlatta2000@gmail.com>
2024-09-05 23:39:36 +0200

    style title,imput mobile

--- src/sections/about/SupportUs/SupportUs.tsx
+++ src/sections/about/SupportUs/SupportUs.tsx
@@ -1,32 +1,55 @@
 'use client';
-import ButtonDonate from '@/components/buttonDonate/buttonDonate';
-import Modal from '@/components/layout/Modal';
+import clsx from 'clsx';
+
 import React, { useState } from 'react';
 
 type SupportUsProps = {};
 
+const subscriptionOptions = [
+  {
+    label: 'Один платіж',
+    value: 'one_time',
+  },
+  {
+    label: 'Щомісячно',
+    value: 'monthly',
+  },
+];
+const currencyOptions = [
+  { value: 'UAH', label: '₴ UAH', symbol: '₴' },
+  { value: 'USD', label: '$ USD', symbol: '$' },
+
+  { value: 'EUR', label: '€ EUR', symbol: '€' },
+];
+
 const SupportUs = ({}: SupportUsProps) => {
   const [activeButton, setActiveButton] = useState<string>('one_time');
+  const [selectedCurrency, setSelectedCurrency] = useState<string>(
+    currencyOptions[0].value
+  );
+  const [donationAmount, setDonationAmount] = useState<number | ''>('');
   // const [isOpen, setIsOpen] = useState(false);
-  const subscriptionOptions = [
-    {
-      label: 'Один платіж',
-      value: 'one_time',
-    },
-    {
-      label: 'Щомісячно',
-      value: 'monthly',
-    },
-  ];
+  const handlecurrencyChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedCurrency(e.target.value);
+  };
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setDonationAmount(value === '' ? '' : parseFloat(value));
+  };
+  const selectedCurrencySymbol =
+    currencyOptions.find((option) => option.value === selectedCurrency)
+      ?.symbol || '';
   return (
-    <section>
-      <h2 className="uppercase">підтримка</h2>
-      <p>
+    <section className="pb-11">
+      <h2 className="mb-3 text-center font-noteworthy text-5xl font-bold uppercase leading-[58.14px] text-accent">
+        підтримка
+      </h2>
+      <p className="mb-6 text-md leading-[24.38px]">
         Якщо вас надихнув проект і ви бажаєте стати нашим донором, будь ласка,
         пожертвуйте зручну для вас суму.
       </p>
       <form className="mt-7 flex w-full flex-col gap-y-8">
-        <div className="lg:flex-row lg:gap-x-10 flex flex-col justify-between gap-y-3 text-center uppercase">
+        <div className="flex h-11 text-center">
           {subscriptionOptions.map((option) => (
             <label
               className="flex flex-1 cursor-pointer items-center gap-x-2"
@@ -39,21 +62,69 @@ const SupportUs = ({}: SupportUsProps) => {
                 className="hidden"
               />
               <div
-                className={`hover:border-accent-primary w-full rounded-2xl border-2 py-[14px] capitalize focus:border-accent-primary${
+                className={clsx(
+                  `flex h-11 w-full items-center justify-center border text-m font-medium hover:border focus:border-accent`,
                   activeButton === option.value
-                    ? 'border-accent-primary'
-                    : 'border-grey-60'
-                }`}
+                    ? 'border-accent text-accent'
+                    : 'border-gray-form',
+                  option.value === 'one_time'
+                    ? `rounded-bl-btn-radius rounded-tl-btn-radius`
+                    : `rounded-br-btn-radius rounded-tr-btn-radius`
+                )}
               >
                 {option.label}
               </div>
             </label>
           ))}
         </div>
+        {/* Select for currency */}
+        <div className="relative w-full max-w-xs">
+          <select
+            value={selectedCurrency}
+            onChange={handlecurrencyChange}
+            className="border-gray-300 text-gray-700 focus:border-blue-500 focus:ring-blue-500 block w-full appearance-none rounded-lg border bg-white px-4 py-2 pr-8 text-sm leading-tight focus:outline-none focus:ring-2"
+          >
+            {currencyOptions.map((option) => (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            ))}
+          </select>
+          <div className="text-gray-700 pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+            <svg
+              className="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path d="M10 12l-5-5h10l-5 5z" />
+            </svg>
+          </div>
+        </div>
+        {/* Input for amount */}
+        <div className="relative w-full max-w-xs">
+          <input
+            type="number"
+            value={donationAmount}
+            onChange={handleAmountChange}
+            placeholder="Enter amount"
+            className="border-gray-300 text-gray-700 focus:border-blue-500 focus:ring-blue-500 block w-full rounded-lg border px-4 py-2 text-sm leading-tight focus:outline-none focus:ring-2"
+          />
+          {/* Currency symbol */}
+          <div className="text-gray-700 absolute inset-y-0 right-0 flex items-center pr-3">
+            {selectedCurrencySymbol}
+          </div>
+        </div>
       </form>
-      <ButtonDonate />
     </section>
   );
 };
 
 export default SupportUs;
+
+// <ButtonDonate /> call modal
+//  <div
+//                 className={`flex h-11 w-full items-center justify-center rounded-bl-btn-radius rounded-tl-btn-radius border text-m font-medium hover:border focus:border-accent ${
+//                   activeButton === option.value
+//                     ? 'border-accent text-accent'
+//                     : 'border-gray-form'
+//                 }`}
