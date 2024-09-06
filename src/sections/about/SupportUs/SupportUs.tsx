cdf812c0dcfd567fc15c1e4b590e8756c803f4f1
GannaKov <zlatta2000@gmail.com>
2024-09-06 15:37:13 +0200

    donateType styles laptop

--- src/sections/about/SupportUs/SupportUs.tsx
+++ src/sections/about/SupportUs/SupportUs.tsx
@@ -8,11 +8,13 @@ type SupportUsProps = {};
 
 const subscriptionOptions = [
   {
-    label: 'Один платіж',
+    labelMob: 'Один платіж',
+    labelDesktop: 'Одноразовий платіж',
     value: 'one_time',
   },
   {
-    label: 'Щомісячно',
+    labelMob: 'Щомісячно',
+    labelDesktop: 'Щомісячний внесок',
     value: 'monthly',
   },
 ];
@@ -42,85 +44,88 @@ const SupportUs = ({}: SupportUsProps) => {
       ?.symbol || '';
   return (
     <section className="laptop:pb-[100px] pb-[60px]">
-      <h2 className="laptop:hidden mb-3 text-center font-noteworthy text-5xl font-bold uppercase leading-[58.14px] text-accent">
-        підтримка
-      </h2>
-      <h2 className="laptop:block mb-12 hidden text-center font-noteworthy text-8xl font-light uppercase leading-[90.44px] text-accent">
-        підтримайте нас
-      </h2>
-      <p className="laptop:mb-10 laptop:text-lg laptop:leading-[31.69px] mb-6 text-md leading-[24.38px]">
-        Якщо вас надихнув проект і ви бажаєте стати нашим донором, будь ласка,
-        пожертвуйте зручну для вас суму.
-      </p>
-      <form className="mt-7 flex w-full flex-col gap-y-8">
-        {/* Choose subscriptionOption */}
-        <div className="flex h-11 text-center">
-          {subscriptionOptions.map((option) => (
-            <label
-              className="flex flex-1 cursor-pointer items-center gap-x-2"
-              key={option.value}
-            >
-              <input
-                type="checkbox"
-                checked={activeButton === option.value}
-                onChange={() => setActiveButton(option.value)}
-                className="hidden"
-              />
-              <div
-                className={clsx(
-                  `flex h-11 w-full items-center justify-center border text-m font-medium leading-[19.5px]`,
-                  activeButton === option.value
-                    ? 'border-accent text-accent shadow-btn-shadow'
-                    : 'border-gray-form',
-                  option.value === 'one_time'
-                    ? 'rounded-bl-btn-radius rounded-tl-btn-radius'
-                    : 'rounded-br-btn-radius rounded-tr-btn-radius'
-                )}
+      <div className="laptop:w-[908px] m-auto">
+        <h2 className="laptop:hidden mb-3 text-center font-noteworthy text-5xl font-bold uppercase leading-[58.14px] text-accent">
+          підтримка
+        </h2>
+        <h2 className="laptop:block mb-12 hidden text-center font-noteworthy text-8xl font-light uppercase leading-[90.44px] text-accent">
+          підтримайте нас
+        </h2>
+        <p className="laptop:text-center laptop:mb-10 laptop:text-lg laptop:leading-[31.69px] mb-6 text-md leading-[24.38px]">
+          Якщо вас надихнув проект і ви бажаєте стати нашим донором, будь ласка,
+          пожертвуйте зручну для вас суму.
+        </p>
+        <form className="laptop:gap-y-12 mt-7 flex w-full flex-col gap-y-8">
+          {/* Choose subscriptionOption */}
+          <div className="flex text-center">
+            {subscriptionOptions.map((option) => (
+              <label
+                className="flex flex-1 cursor-pointer items-center"
+                key={option.value}
               >
-                <p>{option.label}</p>
-              </div>
-            </label>
-          ))}
-        </div>
-
-        <div className="relative w-full">
-          <input
-            type="number"
-            value={donationAmount}
-            onChange={handleAmountChange}
-            placeholder="Сума внеску"
-            className="block w-full border-b border-solid bg-[transparent] pb-3 text-md leading-[17.07px] placeholder:text-s placeholder:text-[#565656] focus:border-accent focus:outline-none"
-          />
-          {/* Currency */}
-          <div className="absolute bottom-3 right-0 flex font-medium text-body-text">
-            <select
-              value={selectedCurrency}
-              onChange={handlecurrencyChange}
-              className="block w-full appearance-none bg-[transparent] px-4 py-2 pr-8 text-sm leading-[19.5px] focus:outline-none"
-            >
-              {currencyOptions.map((option) => (
-                <option
-                  key={option.value}
-                  value={option.value}
-                  className="bg-[transparent]"
+                <input
+                  type="checkbox"
+                  checked={activeButton === option.value}
+                  onChange={() => setActiveButton(option.value)}
+                  className="hidden"
+                />
+                <div
+                  className={clsx(
+                    `bg-[ #EDEDED ] laptop:text-2xl laptop:leading-[36.57px] laptop:h-[61px] flex h-11 w-full items-center justify-center border text-m font-medium leading-[19.5px]`,
+                    activeButton === option.value
+                      ? 'border-accent text-accent shadow-btn-shadow'
+                      : 'border-gray-form',
+                    option.value === 'one_time'
+                      ? 'rounded-bl-btn-radius rounded-tl-btn-radius'
+                      : 'rounded-br-btn-radius rounded-tr-btn-radius'
+                  )}
                 >
-                  {option.label}
-                </option>
-              ))}
-            </select>
-            <div className="text-gray-700 pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
-              <svg
-                className="fill-current h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
+                  <p className="laptop:hidden">{option.labelMob}</p>
+                  <p className="laptop:block hidden">{option.labelDesktop}</p>
+                </div>
+              </label>
+            ))}
+          </div>
+
+          <div className="relative w-full">
+            <input
+              type="number"
+              value={donationAmount}
+              onChange={handleAmountChange}
+              placeholder="Сума внеску"
+              className="block w-full border-b border-solid bg-[transparent] pb-3 text-md leading-[17.07px] placeholder:text-s placeholder:text-[#565656] focus:border-accent focus:outline-none"
+            />
+            {/* Currency */}
+            <div className="absolute bottom-3 right-0 flex font-medium text-body-text">
+              <select
+                value={selectedCurrency}
+                onChange={handlecurrencyChange}
+                className="block w-full appearance-none bg-[transparent] px-4 py-2 pr-8 text-sm leading-[19.5px] focus:outline-none"
               >
-                <path d="M10 12l-5-5h10l-5 5z" />
-              </svg>
+                {currencyOptions.map((option) => (
+                  <option
+                    key={option.value}
+                    value={option.value}
+                    className="bg-[transparent]"
+                  >
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+              <div className="text-gray-700 pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+                <svg
+                  className="fill-current h-4 w-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M10 12l-5-5h10l-5 5z" />
+                </svg>
+              </div>
             </div>
           </div>
-        </div>
-        <Button>ПІДТРИМАТИ</Button>
-      </form>
+          <Button>ПІДТРИМАТИ</Button>
+        </form>
+      </div>
     </section>
   );
 };
