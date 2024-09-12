6b702e8d9fb1873c2266c765749360c6c0648f27
Vlad <vladislav.95antonov@gmail.com>
2024-09-12 21:10:25 +0300

    fix/adaptive-swiper-fix, add-tablet-styles

--- src/sections/about/Team/Swiper/TeamSwiper.tsx
+++ src/sections/about/Team/Swiper/TeamSwiper.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { A11y, Navigation, Pagination } from 'swiper/modules';
 import { SwiperButtonNext, SwiperButtonPrev } from './SwiperButtons';
@@ -14,86 +13,64 @@ import './swiper.css';
 type TeamSwiperProps = {};
 
 const TeamSwiper = ({}: TeamSwiperProps) => {
-  const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <Swiper
-      modules={[Navigation, Pagination, A11y]}
-      slidesPerView={1}
-      spaceBetween="30"
-      navigation={{
-        prevEl: '.custom-button-prev',
-        nextEl: '.custom-button-next',
-      }}
-      pagination={{
-        el: '.custom-pagination',
-        clickable: true,
-      }}
-      className="relative max-w-full tablet:max-w-[100%] desktop:max-h-[464px]"
-    >
-      {teamMembers.map((member) => (
-        <SwiperSlide key={member.id}>
-          <article className="flex max-w-full flex-col rounded-[16px] bg-swiper-card-background p-3 tablet:max-w-[100%] desktop:max-h-[464px] desktop:flex-wrap desktop:content-center desktop:pb-0">
-            <Image
-              src={member.imageUrl}
-              alt={`member ${member.name}`}
-              priority={true}
-              width={441}
-              height={464}
-              className="relative z-0 pb-4 tablet:m-auto desktop:mr-[25px] desktop:p-0"
-            />
-            <p className="absolute z-10 hidden text-center text-lg font-regular text-white desktop:bottom-[50%] desktop:left-[16%] desktop:inline">
-              місце для Вашого фото
-            </p>
-            <section>
-              <h3 className="pb-1 text-lg font-medium text-body-text tablet:text-center desktop:pb-2 desktop:pt-[42px] desktop:text-left desktop:text-6xl desktop:font-regular">
+    <div className="relative">
+      <Swiper
+        modules={[Navigation, Pagination, A11y]}
+        slidesPerView={1}
+        spaceBetween={16}
+        navigation={{
+          prevEl: '.custom-button-prev',
+          nextEl: '.custom-button-next',
+        }}
+        pagination={{
+          el: '.custom-pagination',
+          clickable: true,
+        }}
+        breakpoints={{
+          0: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 16,
+          },
+          1440: {
+            slidesPerView: 3,
+            spaceBetween: 24,
+          },
+        }}
+        className="w-[343px] tablet:w-[540px] desktop:w-[908px]"
+      >
+        {teamMembers.map((member) => (
+          <SwiperSlide
+            key={member.id}
+            className="tablet:w-[262px] laptop:w-[286.65px]"
+          >
+            <article className="flex flex-col rounded-[16px] bg-swiper-card-background p-3 tablet:bg-light-background tablet:p-0">
+              <Image
+                src={member.imageUrl}
+                alt={`member ${member.name}`}
+                priority={true}
+                width={319}
+                height={372}
+                className="relative h-auto tablet:h-[259px]"
+              />
+              <h3 className="pb-1 pt-3 text-[24px] font-medium leading-[29.26px] text-body-text tablet:text-2xl tablet:leading-[36px]">
                 {member.name}
               </h3>
-              <p className="pb-3 text-m font-regular text-body-text tablet:text-center desktop:pb-6 desktop:text-left desktop:text-m desktop:font-light">
+              <p className="pb-3 text-m font-regular leading-[19.5px] text-body-text tablet:font-light tablet:leading-[20px] laptop:text-s laptop:font-regular laptop:leading-[16px]">
                 {member.role}
               </p>
-              <p className="mb-3 pb-3 text-s font-regular text-body-text tablet:mx-10 tablet:max-w-full desktop:mx-0 desktop:max-w-[442px] desktop:text-sm">
-                {member.description1}
-              </p>
-              {isExpanded && (
-                <>
-                  <p className="pb-3 text-s font-regular text-body-text tablet:mx-10 tablet:max-w-full desktop:mx-0 desktop:max-w-[442px] desktop:text-sm">
-                    {member.description2}
-                  </p>
-                  <p className="mb-3 text-s font-regular text-body-text tablet:mx-10 tablet:max-w-full desktop:mx-0 desktop:max-w-[442px] desktop:text-sm">
-                    {member.description3}
-                  </p>
-                  <a
-                    className="text-m font-medium text-body-text underline tablet:hidden"
-                    onClick={() => setIsExpanded(false)}
-                  >
-                    ЧИТАТИ МЕНШЕ
-                  </a>
-                </>
-              )}
-              {!isExpanded && (
-                <a
-                  className="text-m font-medium text-body-text underline tablet:hidden"
-                  onClick={() => setIsExpanded(true)}
-                >
-                  ЧИТАТИ БІЛЬШЕ
-                </a>
-              )}
-              <div className="hidden tablet:block">
-                <p className="pb-3 text-s font-regular text-body-text tablet:mx-10 tablet:max-w-full desktop:mx-0 desktop:max-w-[442px] desktop:text-sm">
-                  {member.description2}
-                </p>
-                <p className="text-s font-regular text-body-text tablet:mx-10 tablet:max-w-full desktop:mx-0 desktop:max-w-[442px] desktop:text-sm">
-                  {member.description3}
-                </p>
-              </div>
-            </section>
-          </article>
-        </SwiperSlide>
-      ))}
+            </article>
+          </SwiperSlide>
+        ))}
+        <div className="custom-pagination"></div>
+      </Swiper>
       <SwiperButtonPrev />
       <SwiperButtonNext />
-      <div className="custom-pagination"></div>
-    </Swiper>
+    </div>
   );
 };
 
--- src/sections/about/Team/Team.tsx
+++ src/sections/about/Team/Team.tsx
@@ -1,25 +1,18 @@
 'use client';
 import TeamSwiper from './Swiper/TeamSwiper';
+import Button from '@/components/ui/Button';
+import SectionTitle from '@/components/ui/SectionTitle';
 
 const Team = () => {
   return (
-    <section className="mb-[60px] rounded-3xl tablet:bg-swiper-card-background desktop:mb-[100px]">
-      <h2 className="pb-3 text-center font-noteworthy text-5xl font-bold tracking-[1.44px] text-accent tablet:pt-6 desktop:pb-12 desktop:pt-10 desktop:text-8xl desktop:tracking-[2.24px]">
-        КОМАНДА
-      </h2>
+    <section className="tablet:transparent mb-[60px] laptop:mb-[100px]">
+      <SectionTitle children="Команда" className="tablet:mb-6 laptop:mb-12" />
       <TeamSwiper />
-      <section className="mx-auto flex max-w-[343px] flex-col text-center tablet:max-w-[908px] tablet:pb-8 desktop:mt-12 desktop:border-t desktop:border-dashed desktop:border-dashed-border desktop:pb-10 desktop:text-left">
-        <p className="p-5 text-l font-regular text-body-text desktop:pb-2 desktop:pt-6 desktop:text-lg">
+      <section className="m-auto flex flex-col items-center tablet:max-w-[908px] tablet:border-t tablet:border-dashed tablet:border-dashed-border tablet:pb-8 laptop:mt-12 laptop:pb-10">
+        <p className="mb-6 mt-8 text-center text-l font-regular leading-[26px] text-body-text tablet:w-[506px] laptop:w-[908px] laptop:pb-2 laptop:pt-6 laptop:text-lg laptop:leading-[32px]">
           Бажаєш стати частиною нашої місії? Долучайся до команди вже сьогодні!
         </p>
-        <button
-          type="button"
-          title="connect-to-team-button"
-          onClick={() => alert('Good job my friend!')}
-          className="w-[343px] rounded-[24px] border border-accent bg-accent px-6 py-3 text-sm font-medium text-white tablet:self-center desktop:w-[326px] desktop:self-end"
-        >
-          Долучитись
-        </button>
+        <Button children="Приєднатись" className="w-[343px] tablet:w-[326px]" />
       </section>
     </section>
   );
