550138fa541c31892c8f1688036c3369d7e17153
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-09-13 09:21:19 +0300

    fix edit file Team.tsx

--- src/sections/about/Team/Team.tsx
+++ src/sections/about/Team/Team.tsx
@@ -6,13 +6,13 @@ import SectionTitle from '@/components/ui/SectionTitle';
 const Team = () => {
   return (
     <section className="tablet:transparent mb-[60px] laptop:mb-[100px]">
-      <SectionTitle children="Команда" className="tablet:mb-6 laptop:mb-12" />
+      <SectionTitle className="tablet:mb-6 laptop:mb-12">Команда</SectionTitle>
       <TeamSwiper />
       <section className="m-auto flex flex-col items-center tablet:max-w-[908px] tablet:border-t tablet:border-dashed tablet:border-dashed-border tablet:pb-8 laptop:mt-12 laptop:pb-10">
         <p className="mb-6 mt-8 text-center text-l font-regular leading-[26px] text-body-text tablet:w-[506px] laptop:w-[908px] laptop:pb-2 laptop:pt-6 laptop:text-lg laptop:leading-[32px]">
           Бажаєш стати частиною нашої місії? Долучайся до команди вже сьогодні!
         </p>
-        <Button children="Приєднатись" className="w-[343px] tablet:w-[326px]" />
+        <Button className="w-[343px] tablet:w-[326px]">Приєднатись</Button>
       </section>
     </section>
   );
