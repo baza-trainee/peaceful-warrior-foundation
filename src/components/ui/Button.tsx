2aff60410e02a5f1d964deb41dadbb7c439317c2
GannaKov <zlatta2000@gmail.com>
2024-09-13 21:28:10 +0200

    Button for modal

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,17 +5,25 @@ export interface ButtonProps
   disabled?: boolean;
   children: React.ReactNode;
   className?: string;
+  modal?: boolean;
 }
 
-const Button = ({ disabled, children, className, ...rest }: ButtonProps) => {
+const Button = ({
+  disabled,
+  children,
+  modal = false,
+  className,
+  ...rest
+}: ButtonProps) => {
   return (
     <button
       disabled={disabled}
       {...rest}
       className={clsx(
-        'h-[52px] w-[343px] rounded-btn-radius bg-accent text-center text-sm font-medium uppercase leading-[22px] text-[#FFFDFD] shadow-btn-shadow hover:bg-hover tablet:w-[326px]',
+        'rounded-btn-radius bg-accent text-center text-sm font-medium uppercase leading-[22px] text-[#FFFDFD] shadow-btn-shadow hover:bg-hover tablet:w-[326px]',
         'transition-colors duration-200 ease-out',
-
+        modal && 'h-11 w-full tablet:h-[52px]',
+        !modal && 'h-[52px] w-[343px]',
         disabled && 'bg-gray-devider text-gray-border',
         className
       )}
@@ -32,6 +40,18 @@ export default Button;
   type="submit"
   className="m-auto"
   aria-label="Перейти до сторінки платежу"
+>
+  ПІДТРИМАТИ
+</Button>; */
+}
+
+// example for modal
+{
+  /* <Button
+  type="submit"
+  className="m-auto"
+  aria-label="Перейти до сторінки платежу"
+  modal={true} //!!!!!!!!!!!!!!!!!!!!!!!
 >
   ПІДТРИМАТИ
 </Button>; */
