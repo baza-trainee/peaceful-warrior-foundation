2efde08cba60c278df106b1ceb255652b4b021eb
VitaliyPritula <pritula.vitaly94@gmail.com>
2024-09-14 14:32:24 +0300

    created mobile version of the decor

--- src/sections/about/Mission/Mission.tsx
+++ src/sections/about/Mission/Mission.tsx
@@ -12,10 +12,10 @@ const Mission = ({}: MissionProps) => {
         <span className="sm:top-5 sm:left-6 md:top-8 md:left-8 lg:top[-9px] lg:left-10 absolute bottom-[-45px] left-0 h-8 w-8 bg-[url('/assets/images/Mission/icon_quote.svg')] bg-contain bg-center bg-no-repeat leading-loose desktop:left-[-30px]" />
         Повноцінне життя та рівні можливості для тих, хто боронив нашу країну
         від ворога, в сучасному та післявоєнному суспільстві
-        <span className="md:right-[-20px] lg:right-[-30px] absolute right-[-10px] top-[-30px] h-8 w-8 bg-[url('/assets/images/Mission/icon_quote.svg')] bg-contain bg-center bg-no-repeat" />
+        <span className="md:right-[-20px] lg:right-[-30px] absolute right-[-10px] top-[-40px] h-8 w-8 bg-[url('/assets/images/Mission/icon_quote.svg')] bg-contain bg-center bg-no-repeat" />
       </p>
       <div className="relative items-start justify-between text-center tablet:flex">
-        <span className="hidden tablet:block sm:top-5 md:top-8 lg:top[-9px] lg:left-10 absolute bottom-[-30%] left-[-40%] h-[75%] w-[100%] bg-[url('/assets/images/Mission/decor.svg')] bg-contain bg-center bg-no-repeat" />
+        <span className="hidden tablet:block sm:top-5 md:top-8 lg:top[-9px] lg:left-10 absolute bottom-[-30%] left-[-33%] h-[75%] w-[100%] bg-[url('/assets/images/Mission/decor.svg')] bg-contain bg-center bg-no-repeat" />
         <div className="md:translate-x-0 translate-x-5 transform">
           <div className="bg-gray-300 p-4">
             <h3 className="font-normal leading-loose mobile:text-[20px] desktop:text-[30px]">
@@ -47,7 +47,8 @@ const Mission = ({}: MissionProps) => {
           </div>
         </div>
         <span className="hidden tablet:block sm:top-5 sm:right-6 md:top-8 lg:top[-9px] lg:right-10 absolute bottom-[-30%] right-[-17%] h-[75%] w-[50%] scale-x-[-1] transform bg-[url('/assets/images/Mission/decor.svg')] bg-contain bg-center bg-no-repeat leading-loose " />
-        <span className="tablet:hidden sm:top-5 sm:right-6 md:top-8 lg:top[-9px] lg:right-10 absolute bottom-[-30%] right-[-17%] h-[75%] w-[50%] scale-x-[-1] transform bg-[url('/assets/images/Mission/decor-mob.svg')] bg-contain bg-center bg-no-repeat leading-loose " />
+        <span className="tablet:hidden absolute bottom-[-20%] left-[-11%] h-[100%] w-[100%] bg-[url('/assets/images/Mission/decor-mob.svg')] bg-contain bg-left bg-no-repeat -z-10 leading-loose " />
+        
       </div>
     </section>
   );
