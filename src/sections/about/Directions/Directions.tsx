2e870cd394baf630cf40dd2233ebb99d6501532c
Viktoria Vonsovych <vikvons@gmail.com>
2024-09-15 10:30:20 +0200

    fixed styles

--- src/sections/about/Directions/Directions.tsx
+++ src/sections/about/Directions/Directions.tsx
@@ -4,7 +4,9 @@ import styles from './directions.module.css'
 import Button from '@/components/ui/Button';
 import SectionTitle from '@/components/ui/SectionTitle';
 
-const Directions = () => {
+type DirectionsProps = {};
+
+const Directions = ({}: DirectionsProps) => {
   return (
     <>
     <section className="relative pb-[126px] tablet:pb-[157px] laptop:pb-[156px]">
@@ -20,7 +22,7 @@ const Directions = () => {
           className="rounded-t-2xl outline outline-2 outline-gray-border outline-offset-4 w-[321px] max-h-[206px] object-none mb-3.5 grayscale tablet:rounded-tr-[0px] tablet:rounded-l-3xl tablet:w-[188px] tablet:max-h-[156px] tablet:mb-0 desktop:max-h-[185px] desktop:w-[227px]"
           />
           <div className="bg-card-background px-8 py-4 rounded-b-2xl shadow-card-shadow tablet:w-[480px] tablet:px-12 tablet:py-8 tablet:rounded-l-[0px] tablet:rounded-r-3xl desktop:py-9 desktop:w-[675px] desktop:h-[196px]">
-            <h5 className='text-md text-center leading-6 font-medium pb-2 tablet:font-lg tablet:text-left tablet:leading-[32px] tablet:pb-3 desktop:leading-[36px] desktop:text-2xl desktop:font-regular desktop:pb-4'>Протезування</h5>
+            <h5 className='text-md text-center leading-6 font-medium pb-2 tablet:text-lg tablet:text-left tablet:leading-[32px] tablet:pb-3 desktop:leading-[36px] desktop:text-2xl desktop:font-regular desktop:pb-4'>Протезування</h5>
             <p className='text-s text-center leading-4 font-normal tablet:text-m tablet:text-left tablet:leading-[20px] desktop:leading-[24px] desktop:text-md'>Забезпечуємо доступ до кращих лікарів і новітніх медичних розробок, консультації з протезування, допомогу з реабілітацією</p>
             </div>
          </article>
@@ -35,8 +37,8 @@ const Directions = () => {
           height={220}
           className="rounded-t-2xl outline outline-2 outline-gray-border outline-offset-4 max-h-[206px] w-[321px] object-none mb-3.5 grayscale tablet:rounded-tl-[0px] tablet:rounded-r-3xl tablet:w-[188px] tablet:h-[156px] tablet:mb-0 desktop:h-[185px] desktop:w-[227px]"
           />
-          <div className="bg-card-background px-8 py-4 rounded-b-2xl shadow-card-shadow tablet:rounded-r-[0px] tablet:rounded-l-3xl tablet:w-[480px] tablet:px-12 tablet:py-8 desktop:w-[675px] desktop:h-[196px] desktop:py-9 ">
-            <h5 className='text-md text-center leading-6 font-medium pb-2 tablet:font-lg tablet:text-left tablet:leading-[32px] tablet:pb-3 desktop:leading-[36px] desktop:text-2xl desktop:font-regular desktop:pb-4'>Правова допомога</h5>
+          <div className="bg-card-background px-8 py-4 rounded-b-2xl shadow-card-shadow tablet:rounded-r-[0px] tablet:rounded-l-3xl tablet:w-[480px] tablet:px-12 tablet:py-[42px] desktop:w-[675px] desktop:h-[196px] desktop:py-9 ">
+            <h5 className='text-md text-center leading-6 font-medium pb-2 tablet:text-lg tablet:text-left tablet:leading-[32px] tablet:pb-3 desktop:leading-[36px] desktop:text-2xl desktop:font-regular desktop:pb-4'>Правова допомога</h5>
             <p className='text-s text-center leading-4 font-normal tablet:text-m tablet:leading-[20px] tablet:text-left desktop:leading-[24px] desktop:text-md'>Безоплатна первинна правнича допомога з будь-яких правових питань</p>
             </div>
         </article>
@@ -49,10 +51,10 @@ const Directions = () => {
           alt="prosthesis"
           width={331}
           height={220}
-          className="rounded-t-2xl outline outline-2 outline-gray-border outline-offset-4 max-h-[206px] w-[321px] object-none object-top mb-3.5 grayscale tablet:mb-0 tablet:rounded-tr-[0px] tablet:rounded-l-3xl tablet:w-[188px] tablet:h-[156px] desktop:max-h-[185px] desktop:w-[227px]"
+          className="rounded-t-2xl outline outline-2 outline-gray-border outline-offset-4 max-h-[206px] w-[321px] object-none object-top mb-3.5 grayscale tablet:mb-0 tablet:rounded-tr-[0px] tablet:rounded-l-3xl tablet:w-[188px] tablet:h-[156px] desktop:h-[185px] desktop:w-[227px]"
           />
           <div className="bg-card-background px-8 py-4 rounded-b-2xl shadow-card-shadow tablet:w-[480px] tablet:px-12 tablet:py-8 tablet:rounded-l-[0px] tablet:rounded-r-3xl desktop:w-[675px] desktop:h-[196px]">
-            <h5 className='text-md text-center leading-6 font-medium pb-2 tablet:font-lg tablet:leading-[32px] tablet:text-left tablet:pb-3 desktop:leading-[36px] desktop:text-2xl desktop:font-regular desktop:pb-4'>Психологічна допомога</h5>
+            <h5 className='text-md text-center leading-6 font-medium pb-2 tablet:text-lg tablet:leading-[32px] tablet:text-left tablet:pb-3 desktop:leading-[36px] desktop:text-2xl desktop:font-regular desktop:pb-4'>Психологічна допомога</h5>
             <p className='text-s text-center leading-4 font-normal tablet:text-m tablet:leading-[20px] tablet:text-left desktop:leading-[24px] desktop:text-md'>Забезпечуємо психологічну підтримку для допомоги в подоланні стресу після досвіду бойових дій та адаптації до мирного життя</p>
           </div>
         </article>
@@ -60,7 +62,7 @@ const Directions = () => {
         type="submit"
         aria-label="Перейти до сторінки платежу"
         className="m-auto flex flex-row justify-between items-center px-5 py-3 mt-8 tablet:w-[364px] laptop:w-[364px] laptop:mt-[57px]"
-      >
+        >
         <div><span className="text-nowrap text-center">ЗВЕРНУТИСЬ ЗА ДОПОМОГОЮ</span></div>
         <div><span className="hidden tablet:inline-flex"><svg data-testid="geist-icon" fill="none" shapeRendering="geometricPrecision" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" viewBox="0 0 24 24" className="w-[26px] h-[26px]"><path d="M7 17L17 7"></path><path d="M7 7h10v10"></path></svg></span></div>
       </Button>
