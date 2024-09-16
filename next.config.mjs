be851d54e8739cd28d844bfd9057630f00afbc17
Vlad <vladislav.95antonov@gmail.com>
2024-09-16 22:03:01 +0300

    feat/add next intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,36 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
+import createNextIntlPlugin from 'next-intl/plugin';
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-export function webpack(config) {
+const nextConfig = {
+  webpack(config) {
     config.resolve.alias.canvas = false;
     config.module.rules.push({
-        test: /\.(pdf)$/,
-        type: 'asset/resource',
+      test: /\.(pdf)$/,
+      type: 'asset/resource',
     });
 
-    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
     config.module.rules.push(
-        {
-            ...fileLoaderRule,
-            test: /\.svg$/i,
-            resourceQuery: /url/,
-        },
-        {
-            test: /\.svg$/i,
-            issuer: fileLoaderRule.issuer,
-            resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-            use: ['@svgr/webpack'],
-        }
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      }
     );
 
     fileLoaderRule.exclude = /\.svg$/i;
 
     return config;
-}
+  },
+};
+
+export default withNextIntl(nextConfig);
--- src/sections/about/Team/Team.tsx
+++ src/sections/about/Team/Team.tsx
@@ -3,14 +3,19 @@ import TeamSwiper from './Swiper/TeamSwiper';
 import Button from '@/components/ui/Button';
 import SectionTitle from '@/components/ui/SectionTitle';
 
+import { useTranslations } from 'next-intl';
+
 const Team = () => {
+  const t = useTranslations('Team');
   return (
     <section className="tablet:transparent mb-[60px] laptop:mb-[100px]">
-      <SectionTitle className="tablet:mb-6 laptop:mb-12">Команда</SectionTitle>
+      <SectionTitle className="tablet:mb-6 laptop:mb-12">
+        {t('title')}
+      </SectionTitle>
       <TeamSwiper />
       <section className="m-auto flex flex-col items-center tablet:max-w-[908px] tablet:border-t tablet:border-dashed tablet:border-dashed-border tablet:pb-8 laptop:mt-12 laptop:pb-10">
         <p className="mb-6 mt-8 text-center text-l font-regular leading-[26px] text-body-text tablet:w-[506px] laptop:w-[908px] laptop:pb-2 laptop:pt-6 laptop:text-lg laptop:leading-[32px]">
-          Бажаєш стати частиною нашої місії? Долучайся до команди вже сьогодні!
+          {t('join')}
         </p>
         <Button className="w-[343px] tablet:w-[326px]">Приєднатись</Button>
       </section>
