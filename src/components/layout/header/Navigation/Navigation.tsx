0d3c6e1220d333c30f4491b331c2ec9239db2f6b
Vlad <vladislav.95antonov@gmail.com>
2024-09-19 13:36:58 +0300

    fix not found page bug with next intl library

--- src/components/layout/header/Navigation/Navigation.tsx
+++ src/components/layout/header/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Link, usePathname } from '@/navigation';
+import { Link, usePathname } from '@/i18n/routing';
 import clsx from 'clsx';
 import { useTranslations } from 'next-intl';
 
