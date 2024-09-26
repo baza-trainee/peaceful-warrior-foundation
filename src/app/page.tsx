cb7e2018d77756f34e4a2f3dda44517a765be6bb
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-09-26 10:09:37 +0300

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import Team from '@/sections/about/Team/Team';
 export default function Home() {
   return (
     <>
-      {/* <Hero />*/}
+      <Hero />
       <Mission />
       <Directions />
       <Team /> 
