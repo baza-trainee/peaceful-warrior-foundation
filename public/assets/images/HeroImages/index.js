03056b5a2e21682c711647c4ef66d9cfa3640ae6
Serhii Lutsenko <130161910+SergeyLu89@users.noreply.github.com>
2024-09-26 10:12:00 +0300

    added Hero images

---
+++ public/assets/images/HeroImages/index.js
@@ -0,0 +1,6 @@
+import heroMobile from './Group_mobile.png';
+import heroMobileGradient from './Ellipse.png';
+import heroTablet from './Group_tablet.png';
+import heroDesktop from './Group_desktop.png';
+
+export { heroMobile, heroMobileGradient, heroTablet, heroDesktop };
