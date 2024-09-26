7c58c0d499a12dc812344277bbc04fcf69f081ec
NataliaMahera <magera.nataliia@gmail.com>
2024-09-26 10:21:33 +0200

    update: active lang in language toogler

--- src/components/layout/header/LanguageToogler/LanguageToogler.tsx
+++ src/components/layout/header/LanguageToogler/LanguageToogler.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import { useRouter, usePathname } from '@/i18n/routing';
+import { useLocale } from 'next-intl';
 
 export const LanguageToogler = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const currentLocale = useLocale();
 
   const changeLanguage = (locale: any) => {
     router.push(pathname, { locale });
@@ -15,14 +17,17 @@ export const LanguageToogler = () => {
       <button
         onClick={() => changeLanguage('ua')}
         type="button"
-        className="flex cursor-pointer items-center text-m font-medium uppercase leading-[19.5px] text-light-background transition-all duration-300 hover:text-hover tablet:text-sm tablet:font-semibold tablet:leading-[21.94px] tablet:text-body-text"
+        className="flex cursor-pointer items-center text-m font-medium uppercase leading-[19.5px] text-light-background transition-all duration-300 hover:text-accent tablet:text-sm tablet:font-semibold tablet:leading-[21.94px] tablet:text-body-text"
+        style={{ color: currentLocale === 'ua' ? '#115ccd' : '#f0f0f0' }}
       >
-        UK
+        UA
       </button>
       <span className="h-[20px] border-[1px] border-stroke-quotes"></span>
       <button
         onClick={() => changeLanguage('en')}
-        className="flex cursor-pointer items-center text-m font-medium uppercase leading-[19.5px] text-light-background transition-all duration-300 hover:text-hover tablet:text-sm tablet:font-semibold tablet:leading-[21.94px] tablet:text-body-text"
+        type="button"
+        className="flex cursor-pointer items-center text-m font-medium uppercase leading-[19.5px] text-light-background transition-all duration-300 hover:text-accent tablet:text-sm tablet:font-semibold tablet:leading-[21.94px] tablet:text-body-text"
+        style={{ color: currentLocale === 'en' ? '#115ccd' : '#f0f0f0' }}
       >
         EN
       </button>
