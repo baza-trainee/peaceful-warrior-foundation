343579aacca1ca12a94aa11661c66d216ecf9629
GannaKov <zlatta2000@gmail.com>
2025-02-14 00:09:46 +0100

    desk vec 1

--- src/sections/help/Join/Join.tsx
+++ src/sections/help/Join/Join.tsx
@@ -11,7 +11,7 @@ const Join: React.FC<JoinProps> = () => {
   const t = useTranslations('HelpPage.Join');
 
   return (
-    <section className="relative pb-[60px] pt-6 tablet:pb-[130px] tablet:pt-8 desktop:pb-[100px] desktop:pt-8">
+    <section className="relative pb-[60px] pt-6 tablet:pb-[130px] tablet:pt-8 desktop:pb-[180px] desktop:pt-8">
       <div className="mx-auto mb-3 w-[330px] tablet:mb-6 tablet:w-full desktop:mb-12">
         {' '}
         <SectionTitle className="">{t('title')}</SectionTitle>
@@ -22,8 +22,8 @@ const Join: React.FC<JoinProps> = () => {
         </p>
       </div>
       <JoinForm />
-      <div className="-right-5 hidden tablet:absolute tablet:bottom-[90px] tablet:block tablet:h-[82px] tablet:w-[182px] tablet:bg-[url('/assets/images/Help/Join/Vector1.png')] tablet:bg-cover tablet:bg-center tablet:bg-no-repeat desktop:bottom-14 desktop:h-[192px] desktop:w-[1032px]" />
-      <div className="-right-8 hidden tablet:absolute tablet:bottom-[120px] tablet:block tablet:h-[206px] tablet:w-[192px] tablet:bg-[url('/assets/images/Help/Join/Vector2.png')] tablet:bg-cover tablet:bg-center tablet:bg-no-repeat desktop:bottom-14 desktop:h-[192px] desktop:w-[1032px]" />
+      <div className="-right-5 hidden tablet:absolute tablet:bottom-[90px] tablet:block tablet:h-[82px] tablet:w-[182px] tablet:bg-[url('/assets/images/Help/Join/Vector1.png')] tablet:bg-cover tablet:bg-center tablet:bg-no-repeat desktop:bottom-[120px] desktop:right-[100px] desktop:h-[108px] desktop:w-[225px]" />
+      <div className="-right-8 hidden tablet:absolute tablet:bottom-[120px] tablet:block tablet:h-[206px] tablet:w-[192px] tablet:bg-[url('/assets/images/Help/Join/Vector2.png')] tablet:bg-cover tablet:bg-center tablet:bg-no-repeat desktop:bottom-[133px] desktop:right-12 desktop:h-[210px] desktop:w-[442px]" />
       <div className="left-0 hidden tablet:absolute tablet:bottom-[90px] tablet:block tablet:h-[202px] tablet:w-[492px] tablet:bg-[url('/assets/icons/help/join/vect6.svg')] tablet:bg-cover tablet:bg-center tablet:bg-no-repeat desktop:bottom-14 desktop:h-[192px] desktop:w-[1032px]" />
     </section>
   );
