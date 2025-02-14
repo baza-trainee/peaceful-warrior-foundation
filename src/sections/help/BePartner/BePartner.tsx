4df04aba99e4ed3921b914efcacd4a73d118b94b
GannaKov <zlatta2000@gmail.com>
2025-02-14 21:38:27 +0100

    save translate

--- src/sections/help/BePartner/BePartner.tsx
+++ src/sections/help/BePartner/BePartner.tsx
@@ -8,19 +8,16 @@ import BePartnerForm from '@/components/forms/BePartnerForm';
 type BePartnerProps = {};
 
 const BePartner: React.FC<BePartnerProps> = () => {
-  // const t = useTranslations('HelpPage.Join');
+  const t = useTranslations('HelpPage.BePartner');
 
   return (
     <section className="relative mx-auto pb-[60px] tablet:pb-[130px] laptop:max-w-[744px] desktop:max-w-[1440px] desktop:pb-[180px]">
-      {/* {t('title')} */}
       <SectionTitle className="mx-auto mb-3 tablet:mb-6 desktop:mb-12">
-        стати партнером
+        {t('title')}
       </SectionTitle>
       <div className="mb-6 w-full tablet:mx-auto tablet:w-[674px] desktop:mb-10 desktop:w-[859px]">
         <p className="text-center text-sm leading-[22px] text-gray-text tablet:text-l tablet:leading-[26px] desktop:text-lg desktop:leading-8">
-          {/* {t('text')} */}
-          Доєднуйтесь, якщо хочете підтримати наших підопічних. Разом завжди
-          краще, ніж поодинці!
+          {t('text')}
         </p>
       </div>
       <BePartnerForm />
