a0087b8be19d442340b690862e3329a054160494
GannaKov <zlatta2000@gmail.com>
2025-02-17 19:45:28 +0100

    NotFound in app

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import NotFoundContent from '@/components/notFound/NotFoundContent';
+
+export interface NotFoundProps {}
+
+export default function NotFound({}: NotFoundProps) {
+  return (
+    <>
+      <NotFoundContent />
+    </>
+  );
+}
